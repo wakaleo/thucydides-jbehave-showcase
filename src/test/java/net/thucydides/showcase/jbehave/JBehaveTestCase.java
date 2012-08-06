7a239069c9a1070b880d7a8616bc31625f28aaee
John Ferguson Smart <john.smart@wakaleo.com>
2012-08-06 21:12:14 +1000

    Initial version with pending stories

---
+++ src/test/java/net/thucydides/showcase/jbehave/JBehaveTestCase.java
@@ -0,0 +1,7 @@
+package net.thucydides.showcase.jbehave;
+
+import net.thucydides.jbehave.ThucydidesJUnitStories;
+
+public class JBehaveTestCase extends ThucydidesJUnitStories {
+    public JBehaveTestCase() {}
+}
